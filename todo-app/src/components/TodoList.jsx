d589fdb7cd27a4aa932085b06f186f41b32ea422
yash <yashyashwantrao74@gmail.com>
2025-09-24 23:40:34 +0530

    Add functions

--- todo-app/src/components/TodoList.jsx
+++ todo-app/src/components/TodoList.jsx
@@ -1,54 +1,82 @@
-import React,{useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
 const TodoList = () => {
-const [input, setInput] = useState("");
-const [todos, setTodos] = useState([]);
-const [error, setError] = useState(null);
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [editId, setEditId] = useState("");
+  const [completed, setCompleted] = useState(false);
+  const [error, setError] = useState(null);
 
-const addTodos = (e) => {
- e.preventDefault();
- if(!input.trim()){
-    setError("Please fill the box!");
-    return;
- }
- const newTodo  = {
-    id:Date.now(),
-    text:input
- }
- setTodos([...todos,newTodo]);
- setInput("");
-}
+  const addTodos = (e) => {
+    e.preventDefault();
+    if (!input.trim()) {
+      setError("Please fill the box!");
+      return;
+    }
+    const newTodo = {
+      id: Date.now(),
+      text: input,
+      completed:false
+    }
+    if (editId) {
+      setTodos(prev =>
+        prev.map(todo =>
+          todo.id === editId ? { ...todo, text: input } : todo
+        )
+      )
+      setEditId("");
+    } else {
+      setTodos([...todos, newTodo])
+    }
+    setInput("");
+  }
 
-const removeTodo = (id) => {
+  const removeTodo = (id) => {
     setTodos(prev => prev.filter(todo => todo.id !== id));
-}
+  }
+
+  const editTodo = (todo) => {
+    setEditId(todo.id);
+    setInput(todo.text);
+    console.log(todo.id)
+  }
+
+  const toggleCompleted = (id) => {
+    setTodos(prev =>
+      prev.map(todo =>
+        todo.id === id ? {...todo,completed:!todo.completed} : todo
+      )
+    )
+  }
 
   return (
-    <> 
-       <div className="container">
-          <form onSubmit={addTodos}>
-            <input type="text" 
+    <>
+      <div className="container">
+        <form onSubmit={addTodos}>
+          <input type="text"
             placeholder='Enter todo'
             value={input}
-            onChange={(e)=>setInput(e.target.value)}/>
+            onChange={(e) => setInput(e.target.value)} />
 
-            <button type='submit'>Add</button>
-          </form>
-              <div className="todo-list">
-                 <ul>
-                    {todos > 0 ? (
-                        <p>No todos</p>
-                    ) : (
-                        todos.map(todo => (
-                            <li key={todo.id}>
-                                <h1>{todo.text}</h1>
-                                <button onClick={()=>removeTodo(todo.id)}>Delete</button>
-                            </li>
-                        ))
-                    ) }
-                 </ul>
-              </div>
-       </div>
+          <button type='submit'>{editId ? "Update" : "Add"}</button>
+        </form>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        <div className="todo-list">
+          <ul>
+            {todos.length === 0 ? (
+              <p>No todos</p>
+            ) : (
+              todos.map(todo => (
+                <li key={todo.id}>
+                  <h1 style={{ textDecoration: todo.completed ? "line-through" : "" }} onClick={()=>toggleCompleted(todo.id)} >{todo.text}</h1>
+                  <button onClick={() => editTodo(todo)}>Edit</button>
+                  <button onClick={() => removeTodo(todo.id)}>Delete</button>
+                </li>
+              ))
+            )}
+          </ul>
+        </div>
+      </div>
     </>
   )
 }
